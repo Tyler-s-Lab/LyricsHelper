4bce8c8c1ebe08ca06c0073fc6d6ccec7a190392
Tyler True <TylerTrueZ@outlook.com>
2026-07-13 11:35:44 +0800

    Font size scales on text preview window

--- LyricsHelper/WindowText.xaml.cs
+++ LyricsHelper/WindowText.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace LyricsHelper {
 	/// <summary>
@@ -16,6 +17,28 @@ namespace LyricsHelper {
 			set {
 				textbox.Text = value;
 			}
+		}
+
+		private void Textbox_MouseWheel(object sender, MouseWheelEventArgs e) {
+			if (!(Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))) {
+				e.Handled = false;
+				return;
+			}
+
+			if (e.Delta > 0) {
+				if (textbox.FontSize < 256)
+					textbox.FontSize *= 1.05;
+				e.Handled = true;
+				return;
+			}
+			if (e.Delta < 0) {
+				if (textbox.FontSize > 2)
+					textbox.FontSize /= 1.05;
+				e.Handled = true;
+				return;
+			}
+			e.Handled = false;
+			return;
 		}
 	}
 }
