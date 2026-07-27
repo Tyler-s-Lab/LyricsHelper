7095fb7ff90d6b0cd0a70354d03b26bfe6d5049c
Tyler Parret True <mysteryworldgod@outlook.com>
2026-07-27 11:35:58 +0800

    Parse runs correctly

--- LyricsHelper/RubyExtract.cs
+++ LyricsHelper/RubyExtract.cs
@@ -25,51 +25,38 @@ namespace LyricsHelper {
 			if (document.Element(w + "body") is not XElement body) {
 				throw new Exception("XML not including w:body");
 			}
-			var paragraphs = body.Elements(w + "p");
-			foreach (var paragraph in paragraphs) {
-				var runs = paragraph.Elements(w + "r");
-				foreach (var run in runs) {
-					bool isMistake = false; // 带颜色的Ruby表明读音并非正确 + 包含平假名以外的ruby
-					if (run.Element(w + "rPr") is XElement runPreference) {
-						if (runPreference.Element(w + "color") is XElement color) {
-							if (color.Attribute(w + "val") is XAttribute colorVal) {
-								if (colorVal.Value != "000000") {
-									isMistake = true;
-								}
-							}
-						}
-					}
-					if (run.Element(w + "t") is XElement text) {
-						res += text.Value;//.Replace("“", null);
-					}
-					else if (run.Element(w + "ruby") is XElement ruby) {
-						var rubyText = ruby.Element(w + "rt")?.Element(w + "r")?.Element(w + "t");
-						var rubyBase = ruby.Element(w + "rubyBase")?.Element(w + "r")?.Element(w + "t");
 
-						if (rubyText != null) {
-							isMistake = isMistake || rubyText.Value.Any(c => GetTextType(c) != TextType.Hiragana);
-						}
+			foreach (var paragraph in body.Elements(w + "p")) {
+				foreach (var run in paragraph.Elements(w + "r")) {
+					bool isMistakeRun = false; // 带颜色的Ruby表明读音并非正确 + 包含平假名以外的ruby
 
-						if (isMistake) {
-							if (rubyBase != null) {
-								res += $"[{rubyBase.Value}]";
+					foreach (var ele in run.Elements()) {
+						if (ele.Name == w + "rPr" && ele is XElement runPreference) {
+							bool isBlack = runPreference.Element(w + "color")?.Attribute(w + "val")?.Value?.Equals("000000") ?? true;
+							isMistakeRun = isMistakeRun || !isBlack;
+						}
+						else if (ele.Name == w + "t" && ele is XElement text) {
+							res += text.Value;//.Replace("“", null);
+						}
+						else if (ele.Name == w + "ruby" && ele is XElement ruby) {
+							var rubyText = ruby.Element(w + "rt")?.Element(w + "r")?.Element(w + "t")?.Value;
+							var rubyBase = ruby.Element(w + "rubyBase")?.Element(w + "r")?.Element(w + "t")?.Value;
+
+							bool isMistakeRuby = rubyText?.Any(c => GetTextType(c) != TextType.Hiragana) ?? false;
+
+							if (isMistakeRun || isMistakeRuby) {
+								res += $"[{rubyBase}]";
+								res += $"({rubyText})";
 							}
-							if (rubyText != null) {
-								res += $"({rubyText.Value})";
+							else {
+								res += rubyText ?? rubyBase;
 							}
 						}
-						else {
-							if (rubyText != null) {
-								res += rubyText.Value;
-							}
-							else if (rubyBase != null) {
-								res += rubyBase.Value;
-							}
+						else if (ele.Name == w + "br") {
+							res += '\n';
 						}
 					}
-					else if (run.Element(w + "br") is XElement _) {
-						res += '\n';
-					}
+
 				}
 				res += Environment.NewLine;
 			}
